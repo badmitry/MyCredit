57242c3bf3b3aa383fd2bf20a0a64a23d2decf02
badmitry <dmitrij88@bk.ru>
2021-12-10 16:28:24 +0300

    Выпилил тестовую учетку

--- app/src/main/java/com/badmitry/vtbhackaton/fragments/FragmentSigning.kt
+++ app/src/main/java/com/badmitry/vtbhackaton/fragments/FragmentSigning.kt
@@ -18,13 +18,6 @@ import javax.inject.Inject
 class FragmentSigning : BaseFragment() {
     private lateinit var binding: FragmentSigningBinding
     private lateinit var viewModel: FragmentSigningViewModel
-
-    private val appClientId = "920cb60cce58e560c4463b0c2e2a9970"
-    private val appClientSecret = "157baf586bde3eb6795dd15727107986"
-    private val redirectUri = "https://badmitry.com"
-
-    private val testLogin = "team21@app.hackaton.bankingapi.ru"
-    private val testPassword = "jGW2R1Gi"
     private val CODE_PERMISSION_LOCATION = 1100
 
     @Inject
@@ -66,11 +59,6 @@ class FragmentSigning : BaseFragment() {
                 binding.etLogin.text.toString(),
                 binding.etPassword.text.toString()
             )
-        }
-
-        binding.btnTestAccount.setOnClickListener {
-            binding.etLogin.setText(testLogin)
-            binding.etPassword.setText(testPassword)
         }
     }
 
